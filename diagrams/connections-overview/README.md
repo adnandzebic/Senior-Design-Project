541c2c0b45234c08bf75c718606e0c4f54334a9d
Adnan Dzebic <adnandzebic@gmail.com>
2015-10-20 10:24:15 -0700

    Added our first subclass and a simple connections diagram

---
+++ diagrams/connections-overview/README.md
@@ -0,0 +1,10 @@
+# Connections Overview Diagram
+
+Install blockdiag using pip:
+`$ sudo pip install blockdiag`
+
+To generate an svg image:
+`$ blockdiag -f /usr/share/....`
+
+Result:
+![][connections-overview.svg]
---
+++ our-app/app/src/main/java/io/particle/android/sdk/smartmirror/SmartMirrorListFragment.java
@@ -0,0 +1,9 @@
+package io.particle.android.sdk.smartmirror;
+
+import io.particle.android.sdk.ui.DeviceListFragment;
+
+/**
+ * Created by adnan on 10/20/15.
+ */
+public class SmartMirrorListFragment extends DeviceListFragment {
+}
