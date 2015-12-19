2c7adc016333a49af11a4df2d4a050e2abcb08c9
Adnan Dzebic <adnandzebic@gmail.com>
2015-12-19 01:09:22 -0800

    added php stuff

---
+++ http/README.md
@@ -0,0 +1,10 @@
+MagicMirror
+===========
+
+The super magic interface of my personal Magic Mirror. More information about this project can be found on my [blog](http://michaelteeuw.nl/tagged/magicmirror).
+
+Runs as a php script on a web server with basically no external dependencies. Can use socket.io for XBEE integration, but isn't required for basic functionality.
+
+Modify js/config.js to change some general variables (language, wather location, compliments, news feed RSS) and calendar.php to add your own ICS calendar
+
+To use the OpenWeatherMap API, you'll need a free API key. Checkout [this blogpost](http://michaelteeuw.nl/post/131504229357/what-happened-to-the-weather) for more information.
---
+++ http/js/config.js
@@ -0,0 +1,39 @@
+// for navigator language
+// var lang = window.navigator.language;
+// you can change the language
+var lang = 'en';
+
+//change weather params here:
+//units: metric or imperial
+var weatherParams = {
+    'q':'San Jose,US',
+    'units':'imperial',
+    'lang':lang,
+    'APPID':'35f135d973d339c1ad9178f5e15285f9'
+};
+
+//var feed = 'http://feeds.nos.nl/nosjournaal?format=rss';
+//var feed = 'http://www.nu.nl/feeds/rss/achterklap.rss';
+//var feed = 'http://www.nu.nl/feeds/rss/opmerkelijk.rss';
+//var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+//var feed = 'http://www.nba.com/lakers/rss.xml';
+var feed = 'http://feeds.feedburner.com/IeeeSpectrumSemiconductors?format=xml';
+
+// compliments:
+var morning = [
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
+        ];
+        
+var afternoon = [
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
+        ];
+       
+var evening = [
+            'You can\'t spell Geek without EE.',
+            'You look nice!',
+            'Hi, sexy!'
+        ];
